1b1166822757ee8eb93bfd5eb081f953c278e176
Erick <hearteric57@gmail.com>
2025-07-12 17:38:34 +0300

    fix: user cancelled authentication flow error

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -2,10 +2,13 @@ import 'package:academia/config/flavor.dart';
 import 'package:academia/core/core.dart';
 import 'package:academia/database/database.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+import 'package:logger/logger.dart';
 
 class AuthRemoteDatasource {
   final FlavorConfig flavorConfig;
+  final Logger _logger = Logger();
 
   AuthRemoteDatasource({required this.flavorConfig});
 
@@ -16,6 +19,8 @@ class AuthRemoteDatasource {
         callbackUrlScheme: "academia",
         options: FlutterWebAuth2Options(
           windowName: "Academia | Authentication",
+          silentAuth: true,
+          timeout: 2000,
         ),
       );
 
@@ -31,7 +36,16 @@ class AuthRemoteDatasource {
           updatedAt: DateTime.now(),
         ),
       );
+    } on PlatformException catch (pe) {
+      _logger.e("Platform exception occurred", error: pe);
+      return left(
+        AuthenticationFailure(
+          message: "You cancelled the authentication flow",
+          error: pe,
+        ),
+      );
     } catch (e) {
+      _logger.e("Failed to authenticate with google", error: e);
       return left(
         AuthenticationFailure(
           message: "Something went wrong while trying to authenticate you",
--- lib/features/auth/presentation/views/auth_screen.dart
+++ lib/features/auth/presentation/views/auth_screen.dart
@@ -15,95 +15,121 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        minimum: EdgeInsets.all(12),
-        child: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 320),
-            child: Column(
-              spacing: 2,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Image.asset("assets/icons/academia.png", width: 60),
-                ),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Oops! ${state.message}"),
+                behavior: SnackBarBehavior.floating,
+                width: MediaQuery.of(context).size.width * 0.75,
+              ),
+            );
+            return;
+          }
 
-                Text(
-                  "Your school life. Fun.",
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    fontWeight: FontWeight.w600,
+          if (state is AuthAuthenticated) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Welcome!"),
+                behavior: SnackBarBehavior.floating,
+                width: MediaQuery.of(context).size.width * 0.75,
+              ),
+            );
+            return;
+          }
+        },
+        builder: (context, state) => SafeArea(
+          minimum: EdgeInsets.all(12),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 320),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Image.asset("assets/icons/academia.png", width: 60),
                   ),
-                ),
-                Text(
-                  "Login to your Academia account.",
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.outlineVariant,
+
+                  Text(
+                    "Your school life. Fun.",
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
+                  Text(
+                    "Login to your Academia account.",
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                    ),
+                  ),
 
-                SizedBox(height: 16),
-                OutlinedButton.icon(
-                  iconAlignment: IconAlignment.start,
-                  onPressed: () async {
-                    BlocProvider.of<AuthBloc>(
-                      context,
-                    ).add(AuthSignInWithGoogleEvent());
-                  },
-                  label: Text("Continue with Google"),
-                  icon: Icon(FontAwesome.google_brand),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  label: Text("Continue with Apple"),
-                  icon: Icon(FontAwesome.apple_brand),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  label: Text("Continue with Microsoft"),
-                  icon: Icon(FontAwesome.microsoft_brand),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  label: Text("Continue with Github"),
-                  icon: Icon(FontAwesome.github_brand),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  label: Text("Continue with Spotify"),
-                  icon: Icon(FontAwesome.spotify_brand),
-                ),
+                  SizedBox(height: 16),
+                  OutlinedButton.icon(
+                    iconAlignment: IconAlignment.start,
+                    onPressed: () async {
+                      BlocProvider.of<AuthBloc>(
+                        context,
+                      ).add(AuthSignInWithGoogleEvent());
+                    },
+                    label: Text("Continue with Google"),
+                    icon: Icon(FontAwesome.google_brand),
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: () {},
+                    label: Text("Continue with Apple"),
+                    icon: Icon(FontAwesome.apple_brand),
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: () {},
+                    label: Text("Continue with Microsoft"),
+                    icon: Icon(FontAwesome.microsoft_brand),
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: () {},
+                    label: Text("Continue with Github"),
+                    icon: Icon(FontAwesome.github_brand),
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: () {},
+                    label: Text("Continue with Spotify"),
+                    icon: Icon(FontAwesome.spotify_brand),
+                  ),
 
-                SizedBox(height: 22),
-                Text.rich(
-                  TextSpan(
-                    text:
-                        "By continuing, you acknowledge that you understand and agree to the",
-                    children: [
-                      TextSpan(
-                        text: "Terms & conditions",
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.primary,
-                          decoration: TextDecoration.underline,
+                  SizedBox(height: 22),
+                  Text.rich(
+                    TextSpan(
+                      text:
+                          "By continuing, you acknowledge that you understand and agree to Academia's ",
+                      children: [
+                        TextSpan(
+                          text: "Terms & conditions",
+                          style: Theme.of(context).textTheme.bodyMedium
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                decoration: TextDecoration.underline,
+                              ),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
                         ),
-                        recognizer: TapGestureRecognizer()..onTap = () {},
-                      ),
-                      const TextSpan(text: " and "),
-                      TextSpan(
-                        text: "Privacy Policy",
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.primary,
-                          decoration: TextDecoration.underline,
+                        const TextSpan(text: " and "),
+                        TextSpan(
+                          text: "Privacy Policy",
+                          style: Theme.of(context).textTheme.bodyMedium
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                decoration: TextDecoration.underline,
+                              ),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
                         ),
-                        recognizer: TapGestureRecognizer()..onTap = () {},
-                      ),
-                    ],
+                      ],
+                    ),
+                    textAlign: TextAlign.left,
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
-                  textAlign: TextAlign.left,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
