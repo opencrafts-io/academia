c98c0ea8e88aa27cd9f81db69db8722927548e71
Eugene600 <ugowach160@gmail.com>
2025-07-18 11:56:38 +0300

    feat: setup folders and file structure for sherehe feature

---
+++ lib/features/sherehe/presentation/screens/sherehe_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShereheHome extends StatefulWidget {
+  const ShereheHome({super.key});
+
+  @override
+  State<ShereheHome> createState() => _ShereheHomeState();
+}
+
+class _ShereheHomeState extends State<ShereheHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
