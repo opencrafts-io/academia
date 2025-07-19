da312bd8594413f53e098491bce5c08c17955769
Eugene600 <ugowach160@gmail.com>
2025-07-20 01:34:24 +0300

    feat: Added sherehe's home UI

--- lib/features/sherehe/presentation/screens/sherehe_home.dart
+++ lib/features/sherehe/presentation/screens/sherehe_home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+
+import '../widgets/event_card.dart';
 
 class ShereheHome extends StatefulWidget {
   const ShereheHome({super.key});
@@ -8,8 +12,195 @@ class ShereheHome extends StatefulWidget {
 }
 
 class _ShereheHomeState extends State<ShereheHome> {
+  final events = [
+    {
+      "imagePath": 'assets/images/gaelle-marcel-vrkSVpOwchk-unsplash.jpg',
+      "title": 'Gender Reveal Party',
+      "location": 'Kryptons Apartments, Athi River',
+      "date": 'Sat, Jul 20',
+      "time": '2:00 PM',
+    },
+    {
+      "imagePath": 'assets/images/scott-warman-rrYF1RfotSM-unsplash.jpg',
+      "title": 'Cocktail Thursday',
+      "location": 'Pine Breeze Gateway, Athi River',
+      "date": 'Thu, Jul 25',
+      "time": '7:30 PM',
+    },
+    {
+      "imagePath": 'assets/images/lee-blanchflower-1dW1vEJLlCQ-unsplash.jpg',
+      "title": 'Freshas Night',
+      "location": 'Sabina Joy, Nairobi CBD',
+      "date": 'Fri, Jul 26',
+      "time": '9:00 PM',
+    },
+    {
+      "imagePath": 'assets/images/al-elmes-ULHxWq8reao-unsplash.jpg',
+      "title": 'Beach Festival',
+      "location": 'Heri Homes, Athi River',
+      "date": 'Sun, Aug 4',
+      "time": '12:00 PM',
+    },
+    {
+      "imagePath": 'assets/images/lavi-perchik-FCPV_n0lOxc-unsplash.jpg',
+      "title": 'Singles Night',
+      "location": 'Nairobi Street Kitchen, Westlands',
+      "date": 'Fri, Aug 9',
+      "time": '8:00 PM',
+    },
+    {
+      "imagePath": 'assets/images/med-mhamdi-mH_E0K581Yk-unsplash.jpg',
+      "title": 'AfroBeats Night',
+      "location": 'Captains Lounge, Mombasa Rd',
+      "date": 'Sat, Aug 10',
+      "time": '10:00 PM',
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Sherehe")));
+    var theme = Theme.of(context);
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200.0,
+            pinned: true,
+            leading: IconButton(
+              onPressed: () => context.pop(),
+              icon: Icon(Icons.arrow_back),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                'assets/images/diversity.jpg',
+                fit: BoxFit.cover,
+              ),
+              title: Text("Sherehe"),
+            ),
+          ),
+
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 12.0,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search events, places...',
+                  prefixIcon: Icon(Icons.search),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 14.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      // Clear search logic to be implemented later
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // SliverToBoxAdapter(
+          //   child: Column(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Padding(
+          //         padding: const EdgeInsets.symmetric(
+          //           horizontal: 16.0,
+          //           vertical: 8.0,
+          //         ),
+          //         child: Row(
+          //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //           children: [
+          //             Text(
+          //               'Trending Events',
+          //               style: theme.textTheme.headlineSmall?.copyWith(
+          //                 fontWeight: FontWeight.bold,
+          //               ),
+          //             ),
+          //             TextButton(
+          //               onPressed: () {},
+          //               child: Text(
+          //                 "View all",
+          //                 style: theme.textTheme.labelLarge,
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       ),
+          //       CarouselSlider(
+          //         options: CarouselOptions(
+          //           height: 400.0,
+          //           enlargeCenterPage: true,
+          //           enableInfiniteScroll: false,
+          //           viewportFraction: 0.8,
+          //         ),
+          //         items: [
+          //           EventCard(
+          //             imagePath:
+          //                 'assets/images/gaelle-marcel-vrkSVpOwchk-unsplash.jpg',
+          //             title: 'Gender Reveal Party',
+          //             location: 'Kryptons Apartments, Athi River',
+          //             date: 'Sat, Jul 20',
+          //             time: '2:00 PM',
+          //           ),
+          //           EventCard(
+          //             imagePath:
+          //                 'assets/images/scott-warman-rrYF1RfotSM-unsplash.jpg',
+          //             title: 'Cocktail Thursday',
+          //             location: 'Pine Breeze Gateway, Athi River',
+          //             date: 'Thu, Jul 25',
+          //             time: '7:30 PM',
+          //           ),
+          //           EventCard(
+          //             imagePath:
+          //                 'assets/images/lee-blanchflower-1dW1vEJLlCQ-unsplash.jpg',
+          //             title: 'Freshas Night',
+          //             location: 'Sabina Joy, Nairobi CBD',
+          //             date: 'Fri, Jul 26',
+          //             time: '9:00 PM',
+          //           ),
+          //         ],
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 10.0,
+            ),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                if (index == 0) {
+                  return Text(
+                    'Upcoming Events',
+                    style: theme.textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                }
+
+                final  event = events[index - 1];
+                return Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 12.0,
+                  ), // space between cards
+                  child: EventCard(
+                    imagePath: event['imagePath'] ?? "Unknown Image",
+                    title: event['title'] ?? "Unknown title",
+                    location: event['location'] ?? "Unknown Location",
+                    date: event['date'] ?? "Unknown date",
+                    time: event['time'] ?? "Unknown time",
+                  ),
+                );
+              }, childCount: events.length + 1),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/features/sherehe/presentation/widgets/event_card.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class EventCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String location;
+  final String time;
+  final String date;
+  final VoidCallback? onTap;
+
+  const EventCard({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.location,
+    required this.time,
+    required this.date,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              imagePath,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+
+            // Details section
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: theme.textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: theme.colorScheme.onSurface,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        size: 16,
+                        color: theme.colorScheme.primary,
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        location,
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: theme.colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4),
+
+                  // Date
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.calendar_today,
+                        size: 16,
+                        color: theme.colorScheme.primary,
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        date,
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: theme.colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4),
+
+                  // Time
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.access_time,
+                        size: 16,
+                        color: theme.colorScheme.primary,
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        time,
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: theme.colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
