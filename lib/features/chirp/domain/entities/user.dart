9f44c9e2a4eeca1ddcd23b303da57562bf136f2a
NgigiN <ngigi.nyongo@gmail.com>
2025-07-19 03:05:49 +0300

    added an avatar field for users

--- lib/features/chirp/domain/entities/user.dart
+++ lib/features/chirp/domain/entities/user.dart
@@ -4,9 +4,15 @@ class User extends Equatable {
   final String id;
   final String name;
   final String email;
+  final String? avatar;
 
-  const User({required this.id, required this.name, required this.email});
+  const User({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.avatar,
+  });
 
   @override
-  List<Object?> get props => [id, name, email];
+  List<Object?> get props => [id, name, email, avatar];
 }
