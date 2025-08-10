ebf6fe9be7a29ed19eaae6f7dd141509a055e3ee
Erick <hearteric57@gmail.com>
2025-08-10 19:46:37 +0300

    chore: bumped up build number and verson to 2025.8.10+0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "2025.7.28+0"
+        versionCode = 2
+        versionName = "2025.8.10+0"
     }
 
     signingConfigs {
