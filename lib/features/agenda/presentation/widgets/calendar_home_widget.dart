273768f38b197f661688ad99e707328d4a418435
Erick <hearteric57@gmail.com>
2025-08-12 20:05:00 +0300

    chore: made the calendar page tappable

--- lib/features/agenda/presentation/widgets/calendar_home_widget.dart
+++ lib/features/agenda/presentation/widgets/calendar_home_widget.dart
@@ -10,11 +10,57 @@ class CalendarHomeWidget extends StatefulWidget {
 }
 
 class _CalendarHomeWidgetState extends State<CalendarHomeWidget> {
+  DateTime? _selectedDay;
   @override
   Widget build(BuildContext context) {
     return Container(
       constraints: BoxConstraints(maxWidth: ResponsiveBreakPoints.mobile),
       child: TableCalendar(
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+          });
+          // showModalBottomSheet(
+          //   context: context,
+          //   builder: (context) => Center(child: Text("Hi")),
+          // );
+        },
+
+        calendarStyle: CalendarStyle(
+          isTodayHighlighted: true,
+          todayDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Theme.of(context).colorScheme.primary),
+          ),
+          selectedDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            border: Border.all(
+              color: Theme.of(context).colorScheme.primary,
+              width: 2,
+              style: BorderStyle.solid,
+            ),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          defaultDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          weekendDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          outsideDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(16),
+          ),
+
+          selectedTextStyle:
+              Theme.of(context).textTheme.bodyMedium ??
+              const TextStyle(color: Colors.white),
+        ),
+        selectedDayPredicate: (date) => isSameDay(date, _selectedDay),
         focusedDay: DateTime.now(),
         firstDay: DateTime(2025),
         lastDay: DateTime(2080),
