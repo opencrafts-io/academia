6c12c691f57bddd6b1205962071e6c6919a7ceae
Erick <hearteric57@gmail.com>
2025-09-14 14:23:59 +0300

    chore: added validation features to link your school account

--- lib/features/magnet/presentation/views/magnet_auth_screen.dart
+++ lib/features/magnet/presentation/views/magnet_auth_screen.dart
@@ -19,6 +19,9 @@ class MagnetAuthScreen extends StatefulWidget {
 class _MagnetAuthScreenState extends State<MagnetAuthScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final formState = GlobalKey<FormState>();
+  bool _showPassword = false;
+
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator.adaptive(
@@ -67,10 +70,9 @@ class _MagnetAuthScreenState extends State<MagnetAuthScreen> {
             BlocListener<MagnetBloc, MagnetState>(
               listener: (context, state) {
                 if (state is MagnetNotSupportedState) {
-                print(widget.institutionID);
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
-                      duration: Duration(seconds: 30),
+                      duration: Duration(seconds: 10),
                       showCloseIcon: true,
                       content: Text(
                         "Your institution is unsupported on Academia."
@@ -81,154 +83,210 @@ class _MagnetAuthScreenState extends State<MagnetAuthScreen> {
                   );
                   return;
                 }
+                if (state is MagnetErrorState) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      duration: Duration(seconds: 10),
+                      showCloseIcon: true,
+                      content: Text(state.error),
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                  return;
+                }
               },
             ),
           ],
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar.large(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Link your school account."),
-                    Text(
-                      "Powered by magnet",
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ],
+          child: Form(
+            key: formState,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar.large(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Link your school account."),
+                      Text(
+                        "Powered by magnet",
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
 
-              SliverPadding(
-                padding: EdgeInsetsGeometry.all(12),
-                sliver: MultiSliver(
-                  children: [
-                    BlocBuilder<InstitutionBloc, InstitutionState>(
-                      builder: (context, state) {
-                        if (state is InstitutionLoadedState) {
-                          final institution = state.institutions.firstWhere(
-                            (inst) =>
-                                inst.institutionId == widget.institutionID,
-                          );
+                SliverPadding(
+                  padding: EdgeInsetsGeometry.all(12),
+                  sliver: MultiSliver(
+                    children: [
+                      BlocBuilder<InstitutionBloc, InstitutionState>(
+                        builder: (context, state) {
+                          if (state is InstitutionLoadedState) {
+                            final institution = state.institutions.firstWhere(
+                              (inst) =>
+                                  inst.institutionId == widget.institutionID,
+                            );
 
-                          return Card.outlined(
-                            child: ListTile(
-                              title: Text(institution.name),
-                              subtitle: Text(
-                                "Provide credentials for this institution",
-                              ),
-                              trailing: IconButton(
-                                onPressed: () async {
-                                  final url = Uri.parse(
-                                    "https://${institution.domains?.first ?? ''}",
-                                  );
-                                  if (await canLaunchUrl(url)) {
-                                    await launchUrl(
-                                      url,
-                                      mode: LaunchMode
-                                          .externalApplication, // opens in browser
-                                    );
-                                  } else {
-                                    throw 'Could not launch $url';
-                                  }
-                                },
-                                icon: Icon(Icons.link),
-                              ),
-                            ),
-                          );
-                        }
-                        return SizedBox.shrink();
-                      },
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      "Please fill in the form below to link your"
-                      " account of the institution shown above",
-                    ),
-                    SizedBox(height: 12),
-                    TextFormField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(
-                        labelText: "Username",
-                        hintText: "Admission Number",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        hintText: "Your school password",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: 22),
-                    BlocBuilder<MagnetBloc, MagnetState>(
-                      builder: (context, state) {
-                        if (state is MagnetLoadingState) {
-                          return Align(
-                            alignment: Alignment.center,
-                            child: CircularProgressIndicator.adaptive(),
-                          );
-                        }
-                        return FilledButton(
-                          onPressed: () {
-                            showAdaptiveDialog(
-                              context: context,
-                              builder: (context) => AlertDialog.adaptive(
-                                title: Text("Just a quick minute"),
-                                content: Text(
-                                  "By continuing, you consent to us processing your school data in line with our policies. You remain responsible for how you use and share this data.",
+                            return Card.outlined(
+                              child: ListTile(
+                                title: Text(institution.name),
+                                subtitle: Text(
+                                  "Provide credentials for this institution",
                                 ),
-                                actions: [
-                                  FilledButton(
-                                    onPressed: () {
-                                      context.read<MagnetBloc>().add(
-                                        LinkMagnetAccountEvent(
-                                          credentials: Credentials(
-                                            username: _usernameController.text,
-                                            password: _passwordController.text,
-                                          ),
-                                          institutionID: widget.institutionID,
-                                        ),
+                                trailing: IconButton(
+                                  onPressed: () async {
+                                    final url = Uri.parse(
+                                      "https://${institution.domains?.first ?? ''}",
+                                    );
+                                    if (await canLaunchUrl(url)) {
+                                      await launchUrl(
+                                        url,
+                                        mode: LaunchMode
+                                            .externalApplication, // opens in browser
                                       );
-                                      context.pop();
-                                    },
-                                    child: Text("Continue"),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      context.pop();
-                                    },
-                                    child: Text("Leave"),
-                                  ),
-                                ],
+                                    } else {
+                                      throw 'Could not launch $url';
+                                    }
+                                  },
+                                  icon: Icon(Icons.link),
+                                ),
                               ),
                             );
-                          },
-                          child: Text("Link your school account"),
-                        );
-                      },
-                    ),
+                          }
+                          return SizedBox.shrink();
+                        },
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Please fill in the form below to link your"
+                        " account of the institution shown above",
+                      ),
+                      SizedBox(height: 12),
+                      TextFormField(
+                        controller: _usernameController,
+                        validator: (inputText) {
+                          if (inputText!.isEmpty) {
+                            return "Please provide a valid username to continue";
+                          }
+                          return null;
+                        },
 
-                    SizedBox(height: 22),
-                    Text(
-                      "Your school data remains secure using millitary"
-                      " grade encryption standards. Not even us can read your school data without your explicit consent. "
-                      "Checkout on our terms of service and privacy policy"
-                      " for more information.",
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ],
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                          hintText: "Admission Number",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      TextFormField(
+                        obscureText: !_showPassword,
+                        validator: (inputText) {
+                          if (inputText!.isEmpty) {
+                            return "Please provide a valid password to continue.";
+                          }
+                          return null;
+                        },
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _showPassword = !_showPassword;
+                              });
+                            },
+                            icon: Icon(
+                              !_showPassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                          ),
+                          labelText: "Password",
+                          hintText: "Your school password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 22),
+                      BlocBuilder<MagnetBloc, MagnetState>(
+                        builder: (context, state) {
+                          if (state is MagnetLoadingState) {
+                            return Align(
+                              alignment: Alignment.center,
+                              child: CircularProgressIndicator.adaptive(),
+                            );
+                          }
+                          return FilledButton(
+                            onPressed: () {
+                              if (!formState.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    showCloseIcon: true,
+                                    content: Text(
+                                      "Please fill the form to continue",
+                                    ),
+                                    behavior: SnackBarBehavior.floating,
+                                  ),
+                                );
+                                return;
+                              }
+                              showAdaptiveDialog(
+                                context: context,
+                                builder: (context) => AlertDialog.adaptive(
+                                  title: Text("Just a quick minute"),
+                                  content: Text(
+                                    "By continuing, you consent to us processing your school data in line with our policies. You remain responsible for how you use and share this data.",
+                                  ),
+                                  actions: [
+                                    FilledButton(
+                                      onPressed: () {
+                                        context.read<MagnetBloc>().add(
+                                          LinkMagnetAccountEvent(
+                                            credentials: Credentials(
+                                              username:
+                                                  _usernameController.text,
+                                              password:
+                                                  _passwordController.text,
+                                            ),
+                                            institutionID: widget.institutionID,
+                                          ),
+                                        );
+                                        context.pop();
+                                      },
+                                      child: Text("Continue"),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        context.pop();
+                                      },
+                                      child: Text("Leave"),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                            child: Text("Link your school account"),
+                          );
+                        },
+                      ),
+
+                      SizedBox(height: 22),
+                      Text(
+                        "Your school data remains secure using millitary"
+                        " grade encryption standards. Not even us can read your school data without your explicit consent. "
+                        "Checkout on our terms of service and privacy policy"
+                        " for more information.",
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
