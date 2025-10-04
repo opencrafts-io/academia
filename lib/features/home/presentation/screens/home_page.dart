f73ad252063f10a62136c9b6a8d8853a6717f141
Erick <hearteric57@gmail.com>
2025-10-04 21:55:45 +0300

    chore: ui entry point for community memberships

--- lib/features/home/presentation/screens/home_page.dart
+++ lib/features/home/presentation/screens/home_page.dart
@@ -14,6 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedDrawerItem = 0;
   @override
   void initState() {
     super.initState();
@@ -22,89 +23,176 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 3,
-      child: RefreshIndicator.adaptive(
-        onRefresh: () async {},
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              pinned: true,
-              floating: true,
-              snap: true,
-              stretch: false,
-              leading: Padding(
-                padding: EdgeInsetsGeometry.all(8),
-                child: Image.asset("assets/icons/academia.png"),
-              ),
-              centerTitle: true,
-              title: GlobalSearchBar(),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    ProfileRoute().push(context);
-                  },
-                  icon: UserAvatar(scallopDepth: 2),
+    return Scaffold(
+      // drawer: NavigationDrawer(
+      //   selectedIndex: selectedDrawerItem,
+      //   onDestinationSelected: (index) {
+      //     setState(() {
+      //       selectedDrawerItem = index;
+      //     });
+      //   },
+      //   header: Image.asset(
+      //     "assets/icons/academia-logo-variant-1.png",
+      //     width: 180,
+      //   ),
+      //   footer: Text("Made with ❤️ by Open Crafts Interactive."),
+      //   elevation: 0,
+      //
+      //   children: [
+      //     Padding(
+      //       padding: EdgeInsets.all(12),
+      //       child: Text(
+      //         "Chirp",
+      //         style: Theme.of(context).textTheme.titleMedium,
+      //       ),
+      //     ),
+      //     NavigationDrawerDestination(
+      //       icon: Icon(Icons.account_box),
+      //       label: Text("Community memberships"),
+      //       // enabled: selectedDrawerItem == 1,
+      //     ),
+      //     NavigationDrawerDestination(
+      //       icon: Icon(Icons.campaign),
+      //       label: Text("Posts"),
+      //     ),
+      //
+      //     Divider(),
+      //   ],
+      // ),
+      body: DefaultTabController(
+        initialIndex: 0,
+        length: 3,
+        child: RefreshIndicator.adaptive(
+          onRefresh: () async {},
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                pinned: true,
+                floating: true,
+                snap: true,
+                stretch: false,
+                leading: Padding(
+                  padding: EdgeInsetsGeometry.all(8),
+                  child: Image.asset("assets/icons/academia.png"),
                 ),
-              ],
-              bottom: TabBar(
-                isScrollable: true,
-                tabAlignment: TabAlignment.center,
-                tabs: [
-                  // Tab(child: Text("Leaderboard")),
-                  Tab(child: Text("For you")),
-                  Tab(child: Text("Chats")),
-                  Tab(child: Text("Sherehe")),
-                ],
-              ),
-            ),
-
-            SliverPinnedHeader(
-              child: BlocConsumer<PermissionCubit, PermissionState>(
-                listener: (context, state) {
-                  if (state is PermissionPermanentlyDenied) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          "You've previously permanently disabled notifications."
-                          " Please re-enable them on the phone settings page.",
+                centerTitle: true,
+                title: GlobalSearchBar(),
+                actions: [
+                  // PopupMenuButton<int>(
+                  //   itemBuilder: (context) => [
+                  //     PopupMenuItem(value: 1, child: Text("hello")),
+                  //   ],
+                  // ),
+                  PopupMenuButton(
+                    child: UserAvatar(scallopDepth: 2),
+                    onSelected: (item) {
+                      switch (item) {
+                        case 1:
+                          ProfileRoute().push(context);
+                          break;
+                        default:
+                      }
+                    },
+                    offset: Offset(-20, 35),
+                    padding: EdgeInsets.all(12),
+                    itemBuilder: (context) => [
+                      PopupMenuItem(
+                        value: 1,
+                        child: Row(
+                          children: [
+                            Icon(Icons.person),
+                            SizedBox(width: 4),
+                            Text("View profile"),
+                          ],
                         ),
-                        behavior: SnackBarBehavior.floating,
                       ),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state is PermissionDenied ||
-                      state is PermissionPermanentlyDenied) {
-                    return PermissionNotificationAlertCard();
-                  }
-                  return SizedBox.shrink();
-                },
-              ),
-            ),
-            SliverFillRemaining(
-              hasScrollBody: true,
-              fillOverscroll: true,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxWidth: ResponsiveBreakPoints.tablet,
-                  ),
-                  child: TabBarView(
-                    children: [
-                      // Center(child: Text("Leaderboard")),
-                      FeedPage(),
-                      ConversationsPage(),
-                      ShereheHome(),
+                      PopupMenuItem(
+                        value: 2,
+                        child: Row(
+                          children: [
+                            Icon(Icons.account_box),
+                            SizedBox(width: 4),
+                            Text("Community Memberships"),
+                          ],
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: 3,
+                        child: Row(
+                          children: [
+                            Icon(Icons.campaign),
+                            SizedBox(width: 4),
+                            Text("Your posts"),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
+                  // IconButton(
+                  //   onPressed: () {
+                  //     ProfileRoute().push(context);
+                  //   },
+                  //   icon: UserAvatar(scallopDepth: 2),
+                  // ),
+                ],
+                bottom: TabBar(
+                  isScrollable: true,
+                  tabAlignment: TabAlignment.center,
+                  tabs: [
+                    // Tab(child: Text("Leaderboard")),
+                    Tab(child: Text("For you")),
+                    Tab(child: Text("Chats")),
+                    Tab(child: Text("Sherehe")),
+                  ],
                 ),
               ),
-            ),
-          ],
+
+              SliverPinnedHeader(
+                child: BlocConsumer<PermissionCubit, PermissionState>(
+                  listener: (context, state) {
+                    if (state is PermissionPermanentlyDenied) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            "You've previously permanently disabled notifications."
+                            " Please re-enable them on the phone settings page.",
+                          ),
+                          behavior: SnackBarBehavior.floating,
+                        ),
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is PermissionDenied ||
+                        state is PermissionPermanentlyDenied) {
+                      return PermissionNotificationAlertCard();
+                    }
+                    return SizedBox.shrink();
+                  },
+                ),
+              ),
+              SliverFillRemaining(
+                hasScrollBody: true,
+                fillOverscroll: true,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxWidth: ResponsiveBreakPoints.tablet,
+                    ),
+                    child: TabBarView(
+                      children: [
+                        // Center(child: Text("Leaderboard")),
+                        FeedPage(),
+                        ConversationsPage(),
+                        ShereheHome(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
