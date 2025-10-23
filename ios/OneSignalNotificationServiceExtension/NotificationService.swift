09db4ac3047a14a7d7904c8553ade27b2a40bff8
Tendwa-T <tirus301@outlook.com>
2025-10-23 22:55:15 +0300

    Had forgotten to add these files

--- ios/OneSignalNotificationServiceExtension/NotificationService.swift
+++ ios/OneSignalNotificationServiceExtension/NotificationService.swift
@@ -6,30 +6,34 @@
 //
 
 import UserNotifications
+import OneSignalExtension
 
-class NotificationService: UNNotificationServiceExtension {
-
+class NotificationService: UNNotificationServiceExtension{
     var contentHandler: ((UNNotificationContent) -> Void)?
+    var receivedRequest: UNNotificationRequest!
     var bestAttemptContent: UNMutableNotificationContent?
-
+    
+    // A few notes...
+    // This extension only runs when `mutable_content` is set
+    // Setting an attachement or action buttons automatically sets the property to true
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.receivedRequest = request
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        self.bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
+        if let bestAttemptContent = bestAttemptContent{
+            // debugging only G
+            print("Running Notification Service Extension")
+            bestAttemptContent.body = "[Modified] " + bestAttemptContent.body
+            OneSignalExtension.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: self.contentHandler)
         }
     }
     
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+        // Hapa either modify the content that comes in ama use the original payload
+        if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent{
+            OneSignalExtension.serviceExtensionTimeWillExpireRequest(self.receivedRequest, with: self.bestAttemptContent)
             contentHandler(bestAttemptContent)
         }
     }
-
 }
