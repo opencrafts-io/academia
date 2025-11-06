03986360c361f3030bb9a21eae0264eac6062e30
Erick <hearteric57@gmail.com>
2025-11-06 16:36:03 +0300

    chore: implemented pull to refresh for leaderboard

--- lib/features/leaderboard/presentation/views/leaderboard_homepage.dart
+++ lib/features/leaderboard/presentation/views/leaderboard_homepage.dart
@@ -49,94 +49,100 @@ class _LeaderboardHomepageState extends State<LeaderboardHomepage>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return CustomScrollView(
-      slivers: [
-        SliverPinnedHeader(
-          child: Card(
-            margin: EdgeInsets.zero,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-            elevation: 0,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Text(
-                "Global Vibe Rank",
-                style: Theme.of(context).textTheme.titleLarge,
+    return RefreshIndicator.adaptive(
+      onRefresh: () async {
+        context.read<LeaderboardBloc>().add(const RefreshLeaderboard());
+      },
+      child: CustomScrollView(
+        slivers: [
+          SliverPinnedHeader(
+            child: Card(
+              margin: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+              elevation: 0,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Text(
+                  "Global Vibe Rank",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
               ),
             ),
           ),
-        ),
-        SliverPadding(
-          padding: EdgeInsets.all(16),
-          sliver: BlocBuilder<LeaderboardBloc, LeaderboardState>(
-            builder: (context, state) {
-              if (state is LeaderboardLoading) {
-                return SliverFillRemaining(
-                  child: Column(children: [Text("Loading")]),
-                );
-              } else if (state is LeaderboardError) {
-                return SliverFillRemaining(
-                  child: Column(
-                    children: [
-                      Text(state.message),
-                      const SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: () => context.read<LeaderboardBloc>().add(
-                          const LoadLeaderboard(),
+          SliverPadding(
+            padding: EdgeInsets.all(16),
+            sliver: BlocBuilder<LeaderboardBloc, LeaderboardState>(
+              builder: (context, state) {
+                if (state is LeaderboardLoading) {
+                  return SliverFillRemaining(
+                    child: Column(children: [Text("Loading")]),
+                  );
+                } else if (state is LeaderboardError) {
+                  return SliverFillRemaining(
+                    child: Column(
+                      children: [
+                        Text(state.message),
+                        const SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () => context.read<LeaderboardBloc>().add(
+                            const LoadLeaderboard(),
+                          ),
+                          child: const Text('Retry'),
                         ),
-                        child: const Text('Retry'),
-                      ),
-                    ],
-                  ),
-                );
-              } else if (state is LeaderboardLoaded) {
-                return SliverList.builder(
-                  itemCount: state.entries.length + (state.hasMore ? 1 : 0),
-                  itemBuilder: (context, index) {
-                    if (index >= state.entries.length) {
-                      return const Center(
-                        child: Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: CircularProgressIndicator(),
+                      ],
+                    ),
+                  );
+                } else if (state is LeaderboardLoaded) {
+                  return SliverList.builder(
+                    itemCount: state.entries.length + (state.hasMore ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      if (index >= state.entries.length) {
+                        return const Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: CircularProgressIndicator(),
+                          ),
+                        );
+                      }
+                      final entry = state.entries[index];
+                      return ListTile(
+                        contentPadding: EdgeInsets.symmetric(vertical: 4),
+                        title: Text(
+                          "${entry.vibeRank}. ${entry.username ?? 'Anon'}",
+                          style: Theme.of(context).listTileTheme.titleTextStyle
+                              ?.copyWith(fontWeight: FontWeight.bold),
                         ),
-                      );
-                    }
-                    final entry = state.entries[index];
-                    return ListTile(
-                      contentPadding: EdgeInsets.symmetric(vertical: 4),
-                      title: Text(
-                        "${entry.vibeRank}. ${entry.username ?? 'Anon'}",
-                        style: Theme.of(context).listTileTheme.titleTextStyle
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      leading: ClipPath(
-                        clipper: ScallopClipper(
-                          scallopDepth: 2,
-                          numberOfScallops: 12,
-                        ),
-                        child: CircleAvatar(
-                          backgroundImage: CachedNetworkImageProvider(
-                            entry.avatarUrl ??
-                                'https://i.pinimg.com/736x/18/b5/b5/18b5b599bb873285bd4def283c0d3c09.jpg',
+                        leading: ClipPath(
+                          clipper: ScallopClipper(
+                            scallopDepth: 2,
+                            numberOfScallops: 12,
+                          ),
+                          child: CircleAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                              entry.avatarUrl ??
+                                  'https://i.pinimg.com/736x/18/b5/b5/18b5b599bb873285bd4def283c0d3c09.jpg',
+                            ),
                           ),
                         ),
-                      ),
 
-                      trailing: Text(
-                        NumberFormat.compact().format(entry.vibePoints),
-                        style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                          fontWeight: FontWeight.w800,
-                          fontFamily: GoogleFonts.poppins().fontFamily,
+                        trailing: Text(
+                          NumberFormat.compact().format(entry.vibePoints),
+                          style: Theme.of(context).textTheme.titleSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.w800,
+                                fontFamily: GoogleFonts.poppins().fontFamily,
+                              ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              }
-              return SliverToBoxAdapter(child: SizedBox.shrink());
-            },
+                      );
+                    },
+                  );
+                }
+                return SliverToBoxAdapter(child: SizedBox.shrink());
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
