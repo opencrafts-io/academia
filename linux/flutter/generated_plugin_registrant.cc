a73ecab4c1c7b8d61f736351bbbe8b7bfa51b7f8
Erick <hearteric57@gmail.com>
2025-11-08 12:54:46 +0300

    chore: setup awesome notifications for android

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,8 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <awesome_notifications/awesome_notifications_plugin.h>
+#include <awesome_notifications_core/awesome_notifications_core_plugin.h>
 #include <desktop_webview_window/desktop_webview_window_plugin.h>
 #include <dynamic_color/dynamic_color_plugin.h>
 #include <file_selector_linux/file_selector_plugin.h>
@@ -14,6 +16,12 @@
 #include <window_to_front/window_to_front_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) awesome_notifications_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "AwesomeNotificationsPlugin");
+  awesome_notifications_plugin_register_with_registrar(awesome_notifications_registrar);
+  g_autoptr(FlPluginRegistrar) awesome_notifications_core_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "AwesomeNotificationsCorePlugin");
+  awesome_notifications_core_plugin_register_with_registrar(awesome_notifications_core_registrar);
   g_autoptr(FlPluginRegistrar) desktop_webview_window_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopWebviewWindowPlugin");
   desktop_webview_window_plugin_register_with_registrar(desktop_webview_window_registrar);
