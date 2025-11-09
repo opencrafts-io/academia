3bfa12a681e28379853a8bb7aa3810bc50d53b2f
Erick <hearteric57@gmail.com>
2025-11-09 11:44:32 +0300

    chore: added a background task to check daily user activity (dummy implementation)
    - More work is needed for user activity

--- lib/background_callback_dispatcher.dart
+++ lib/background_callback_dispatcher.dart
@@ -1,8 +1,16 @@
+import 'package:academia/background_task/daily_login_background_task.dart';
 import 'package:workmanager/workmanager.dart';
 
 @pragma('vm:entry-point')
 void backgroundCallbackDispatcher() {
+  final DailyLoginBackgroundTask dailyLoginBackgroundTask =
+      DailyLoginBackgroundTask();
+
   Workmanager().executeTask((task, input) async {
-    return Future.value(true);
+    if (task == dailyLoginBackgroundTask.taskName) {
+      await dailyLoginBackgroundTask.execute(input);
+      return true;
+    }
+    return Future.value(false);
   });
 }
