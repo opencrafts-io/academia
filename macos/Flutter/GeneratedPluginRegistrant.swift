f6223f3732ea4bb1c72e73ce677c44209b80aa43
Erick <hearteric57@gmail.com>
2025-11-16 19:27:08 +0300

    feat(monetization): setup RevenueCat integration for Android
    - Add RevenueCat SDK dependency
    - iOS implementation deferred due to Linux environment.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,6 +22,7 @@ import flutter_local_notifications
 import flutter_web_auth_2
 import package_info_plus
 import path_provider_foundation
+import purchases_flutter
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
@@ -50,6 +51,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
