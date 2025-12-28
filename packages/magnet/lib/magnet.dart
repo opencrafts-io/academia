f271d247c40168719ade15fa21c9379d6a739b09
Erick <hearteric57@gmail.com>
2025-12-28 16:50:26 +0300

    chore(magnet): exported necessary command and instruction models from the lib

--- packages/magnet/lib/magnet.dart
+++ packages/magnet/lib/magnet.dart
@@ -1,5 +1,9 @@
 export './src/providers/web_view_provider.dart';
 export './src/exceptions/magnet_exception.dart';
 
+export './src/models/scrapping_result.dart';
+export './src/models/scrapping_command.dart';
+export './src/models/scrapping_instruction.dart';
+
 export './src/core/magnet_config.dart';
 export './src/core/magnet_engine.dart';
