50171c52ef822d5761e0af39ef7f116d35c10d0f
Erick <hearteric57@gmail.com>
2026-01-13 09:30:34 +0300

    chore: adds repository for institution scrapping command

---
+++ lib/features/institution/data/repository/institution_scrapping_command_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'package:academia/core/error/failures.dart';
+import 'package:academia/features/institution/data/models/institution_scrapping_command_model_helper.dart';
+import 'package:academia/features/institution/institution.dart';
+import 'package:dartz/dartz.dart';
+import 'package:magnet/magnet.dart';
+
+class InstitutionScrappingCommandRepositoryImpl
+    extends InstitutionCommandRepository {
+  final InstitutionCommandRemoteDatasource institutionCommandRemoteDatasource;
+  final InstitutionCommandLocalDatasource institutionCommandLocalDatasource;
+
+  InstitutionScrappingCommandRepositoryImpl({
+    required this.institutionCommandLocalDatasource,
+    required this.institutionCommandRemoteDatasource,
+  });
+
+  @override
+  Stream<Either<Failure, ScrappingCommand?>>
+  getInstitutionScrappingCommandByInstitutionID({
+    required int institutionID,
+  }) async* {
+    await institutionCommandRemoteDatasource
+        .fetchInstitutionScrappingCommandByInstitution(
+          institutionID: institutionID,
+        );
+    yield* institutionCommandLocalDatasource
+        .watchInstitutionCommandByInstitution(institutionID: institutionID)
+        .map<Either<Failure, ScrappingCommand?>>(
+          (data) => data.fold(
+            (error) => left(error),
+            (command) => right(command?.toEntity()),
+          ),
+        );
+  }
+}
--- lib/features/institution/domain/domain.dart
+++ lib/features/institution/domain/domain.dart
@@ -1,3 +1,4 @@
 export 'entities/entities.dart';
 export 'repository/institution_repository.dart';
+export 'repository/institution_command_repository.dart';
 export 'usecases/usecases.dart';
