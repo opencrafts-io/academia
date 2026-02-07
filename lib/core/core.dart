070c0da875cb604a2fa8e94c40caf4ff1226e5de
Erick <hearteric57@gmail.com>
2026-02-07 07:35:49 +0300

    chore: adds a drift ColorConverter to help with mapping colors to and from drift models

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -7,5 +7,6 @@ export 'clippers/clippers.dart';
 export 'data/paginated_result.dart';
 export 'data/date_time_converter.dart';
 export 'data/json_converter.dart';
+export 'data/color_converter.dart';
 export 'views/video_trimmer.dart';
 export 'widgets/expanding_fab.dart';
