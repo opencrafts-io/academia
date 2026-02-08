eeec8077e6440b9812fc4de112a42bc999dbc5ed
Erick <hearteric57@gmail.com>
2026-02-08 12:04:36 +0300

    chore: generated initial asset gen for project

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,438 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class $PackagesGen {
+  const $PackagesGen();
+
+  /// Directory path: packages/animated_emoji
+  $PackagesAnimatedEmojiGen get animatedEmoji =>
+      const $PackagesAnimatedEmojiGen();
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/academia-logo-variant-1.png
+  AssetGenImage get academiaLogoVariant1 =>
+      const AssetGenImage('assets/icons/academia-logo-variant-1.png');
+
+  /// File path: assets/icons/academia-v2.png
+  AssetGenImage get academiaV2 =>
+      const AssetGenImage('assets/icons/academia-v2.png');
+
+  /// File path: assets/icons/academia.png
+  AssetGenImage get academia =>
+      const AssetGenImage('assets/icons/academia.png');
+
+  /// File path: assets/icons/card.png
+  AssetGenImage get card => const AssetGenImage('assets/icons/card.png');
+
+  /// File path: assets/icons/dodge.jpg
+  AssetGenImage get dodge => const AssetGenImage('assets/icons/dodge.jpg');
+
+  /// File path: assets/icons/exam.png
+  AssetGenImage get exam => const AssetGenImage('assets/icons/exam.png');
+
+  /// File path: assets/icons/fire.png
+  AssetGenImage get fire => const AssetGenImage('assets/icons/fire.png');
+
+  /// File path: assets/icons/gopher.png
+  AssetGenImage get gopher => const AssetGenImage('assets/icons/gopher.png');
+
+  /// File path: assets/icons/locked-badge.png
+  AssetGenImage get lockedBadge =>
+      const AssetGenImage('assets/icons/locked-badge.png');
+
+  /// File path: assets/icons/medal.png
+  AssetGenImage get medal => const AssetGenImage('assets/icons/medal.png');
+
+  /// File path: assets/icons/opencrafts.png
+  AssetGenImage get opencrafts =>
+      const AssetGenImage('assets/icons/opencrafts.png');
+
+  /// File path: assets/icons/todos.png
+  AssetGenImage get todos => const AssetGenImage('assets/icons/todos.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    academiaLogoVariant1,
+    academiaV2,
+    academia,
+    card,
+    dodge,
+    exam,
+    fire,
+    gopher,
+    lockedBadge,
+    medal,
+    opencrafts,
+    todos,
+  ];
+}
+
+class $AssetsIllustrationsGen {
+  const $AssetsIllustrationsGen();
+
+  /// File path: assets/illustrations/authenticate.jpg
+  AssetGenImage get authenticate =>
+      const AssetGenImage('assets/illustrations/authenticate.jpg');
+
+  /// File path: assets/illustrations/classes.jpg
+  AssetGenImage get classes =>
+      const AssetGenImage('assets/illustrations/classes.jpg');
+
+  /// File path: assets/illustrations/community.jpg
+  AssetGenImage get community =>
+      const AssetGenImage('assets/illustrations/community.jpg');
+
+  /// File path: assets/illustrations/exam_timetable.png
+  AssetGenImage get examTimetable =>
+      const AssetGenImage('assets/illustrations/exam_timetable.png');
+
+  /// File path: assets/illustrations/grades.jpg
+  AssetGenImage get grades =>
+      const AssetGenImage('assets/illustrations/grades.jpg');
+
+  /// File path: assets/illustrations/scene 1.png
+  AssetGenImage get scene1 =>
+      const AssetGenImage('assets/illustrations/scene 1.png');
+
+  /// File path: assets/illustrations/scene 10.png
+  AssetGenImage get scene10 =>
+      const AssetGenImage('assets/illustrations/scene 10.png');
+
+  /// File path: assets/illustrations/scene 11.png
+  AssetGenImage get scene11 =>
+      const AssetGenImage('assets/illustrations/scene 11.png');
+
+  /// File path: assets/illustrations/scene 12.png
+  AssetGenImage get scene12 =>
+      const AssetGenImage('assets/illustrations/scene 12.png');
+
+  /// File path: assets/illustrations/scene 13.png
+  AssetGenImage get scene13 =>
+      const AssetGenImage('assets/illustrations/scene 13.png');
+
+  /// File path: assets/illustrations/scene 14.png
+  AssetGenImage get scene14 =>
+      const AssetGenImage('assets/illustrations/scene 14.png');
+
+  /// File path: assets/illustrations/scene 15.png
+  AssetGenImage get scene15 =>
+      const AssetGenImage('assets/illustrations/scene 15.png');
+
+  /// File path: assets/illustrations/scene 2.png
+  AssetGenImage get scene2 =>
+      const AssetGenImage('assets/illustrations/scene 2.png');
+
+  /// File path: assets/illustrations/scene 3.png
+  AssetGenImage get scene3 =>
+      const AssetGenImage('assets/illustrations/scene 3.png');
+
+  /// File path: assets/illustrations/scene 4.png
+  AssetGenImage get scene4 =>
+      const AssetGenImage('assets/illustrations/scene 4.png');
+
+  /// File path: assets/illustrations/scene 5.png
+  AssetGenImage get scene5 =>
+      const AssetGenImage('assets/illustrations/scene 5.png');
+
+  /// File path: assets/illustrations/scene 6.png
+  AssetGenImage get scene6 =>
+      const AssetGenImage('assets/illustrations/scene 6.png');
+
+  /// File path: assets/illustrations/scene 7.png
+  AssetGenImage get scene7 =>
+      const AssetGenImage('assets/illustrations/scene 7.png');
+
+  /// File path: assets/illustrations/scene 8.png
+  AssetGenImage get scene8 =>
+      const AssetGenImage('assets/illustrations/scene 8.png');
+
+  /// File path: assets/illustrations/scene 9.png
+  AssetGenImage get scene9 =>
+      const AssetGenImage('assets/illustrations/scene 9.png');
+
+  /// File path: assets/illustrations/school-fees.jpg
+  AssetGenImage get schoolFees =>
+      const AssetGenImage('assets/illustrations/school-fees.jpg');
+
+  /// File path: assets/illustrations/timetable.jpg
+  AssetGenImage get timetable =>
+      const AssetGenImage('assets/illustrations/timetable.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    authenticate,
+    classes,
+    community,
+    examTimetable,
+    grades,
+    scene1,
+    scene10,
+    scene11,
+    scene12,
+    scene13,
+    scene14,
+    scene15,
+    scene2,
+    scene3,
+    scene4,
+    scene5,
+    scene6,
+    scene7,
+    scene8,
+    scene9,
+    schoolFees,
+    timetable,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/statue.jpg
+  AssetGenImage get statue => const AssetGenImage('assets/images/statue.jpg');
+
+  /// File path: assets/images/tsuyoshi-kozu-pJ_yqFKXHds-unsplash.jpg
+  AssetGenImage get tsuyoshiKozuPJYqFKXHdsUnsplash => const AssetGenImage(
+    'assets/images/tsuyoshi-kozu-pJ_yqFKXHds-unsplash.jpg',
+  );
+
+  /// List of all assets
+  List<AssetGenImage> get values => [statue, tsuyoshiKozuPJYqFKXHdsUnsplash];
+}
+
+class $AssetsLottiesGen {
+  const $AssetsLottiesGen();
+
+  /// File path: assets/lotties/calendar_events.json
+  String get calendarEvents => 'assets/lotties/calendar_events.json';
+
+  /// File path: assets/lotties/chat.json
+  String get chat => 'assets/lotties/chat.json';
+
+  /// File path: assets/lotties/digital-marketing.json
+  String get digitalMarketing => 'assets/lotties/digital-marketing.json';
+
+  /// File path: assets/lotties/google-calendar.json
+  String get googleCalendar => 'assets/lotties/google-calendar.json';
+
+  /// File path: assets/lotties/graduation-certificate.json
+  String get graduationCertificate =>
+      'assets/lotties/graduation-certificate.json';
+
+  /// File path: assets/lotties/organize.json
+  String get organize => 'assets/lotties/organize.json';
+
+  /// File path: assets/lotties/painting.json
+  String get painting => 'assets/lotties/painting.json';
+
+  /// File path: assets/lotties/promotional-staff.json
+  String get promotionalStaff => 'assets/lotties/promotional-staff.json';
+
+  /// File path: assets/lotties/questions.json
+  String get questions => 'assets/lotties/questions.json';
+
+  /// File path: assets/lotties/task-assigning.json
+  String get taskAssigning => 'assets/lotties/task-assigning.json';
+
+  /// File path: assets/lotties/thinking-learning.json
+  String get thinkingLearning => 'assets/lotties/thinking-learning.json';
+
+  /// File path: assets/lotties/under-maintenance.json
+  String get underMaintenance => 'assets/lotties/under-maintenance.json';
+
+  /// List of all assets
+  List<String> get values => [
+    calendarEvents,
+    chat,
+    digitalMarketing,
+    googleCalendar,
+    graduationCertificate,
+    organize,
+    painting,
+    promotionalStaff,
+    questions,
+    taskAssigning,
+    thinkingLearning,
+    underMaintenance,
+  ];
+}
+
+class $AssetsSvgsGen {
+  const $AssetsSvgsGen();
+
+  /// File path: assets/svgs/academia-logo-variant-1.svg
+  String get academiaLogoVariant1 => 'assets/svgs/academia-logo-variant-1.svg';
+
+  /// List of all assets
+  List<String> get values => [academiaLogoVariant1];
+}
+
+class $PackagesAnimatedEmojiGen {
+  const $PackagesAnimatedEmojiGen();
+
+  /// Directory path: packages/animated_emoji/lottie
+  $PackagesAnimatedEmojiLottieGen get lottie =>
+      const $PackagesAnimatedEmojiLottieGen();
+}
+
+class $PackagesAnimatedEmojiLottieGen {
+  const $PackagesAnimatedEmojiLottieGen();
+
+  /// File path: packages/animated_emoji/lottie/callMeHand.json
+  String get callMeHand => 'packages/animated_emoji/lottie/callMeHand.json';
+
+  /// File path: packages/animated_emoji/lottie/fireHeart.json
+  String get fireHeart => 'packages/animated_emoji/lottie/fireHeart.json';
+
+  /// File path: packages/animated_emoji/lottie/hugFace.json
+  String get hugFace => 'packages/animated_emoji/lottie/hugFace.json';
+
+  /// File path: packages/animated_emoji/lottie/nerdFace.json
+  String get nerdFace => 'packages/animated_emoji/lottie/nerdFace.json';
+
+  /// File path: packages/animated_emoji/lottie/owl.json
+  String get owl => 'packages/animated_emoji/lottie/owl.json';
+
+  /// File path: packages/animated_emoji/lottie/pleading.json
+  String get pleading => 'packages/animated_emoji/lottie/pleading.json';
+
+  /// File path: packages/animated_emoji/lottie/sad.json
+  String get sad => 'packages/animated_emoji/lottie/sad.json';
+
+  /// File path: packages/animated_emoji/lottie/seeNoEvilMonkey.json
+  String get seeNoEvilMonkey =>
+      'packages/animated_emoji/lottie/seeNoEvilMonkey.json';
+
+  /// File path: packages/animated_emoji/lottie/smile.json
+  String get smile => 'packages/animated_emoji/lottie/smile.json';
+
+  /// File path: packages/animated_emoji/lottie/thinkingFace.json
+  String get thinkingFace => 'packages/animated_emoji/lottie/thinkingFace.json';
+
+  /// List of all assets
+  List<String> get values => [
+    callMeHand,
+    fireHeart,
+    hugFace,
+    nerdFace,
+    owl,
+    pleading,
+    sad,
+    seeNoEvilMonkey,
+    smile,
+    thinkingFace,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsIllustrationsGen illustrations =
+      $AssetsIllustrationsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLottiesGen lotties = $AssetsLottiesGen();
+  static const $AssetsSvgsGen svgs = $AssetsSvgsGen();
+  static const $PackagesGen packages = $PackagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
