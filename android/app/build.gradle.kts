426aeda0f90f29dfca560cdc4d338323cad4ab0a
Erick <hearteric57@gmail.com>
2026-02-13 08:31:16 +0300

    release 3.0.2+2
    - Adds progess indicator to magnet scraping flow
    - Responsive status on keys saved on the keys management page
    - Bumped up version number to 3.0.2+2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 28
         targetSdk = 36
-        versionCode = 18
+        versionCode = 19
         versionName = "3.0.2"
     }
 
